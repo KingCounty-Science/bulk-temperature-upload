bde349e00bd53f634ff772b310a9d33c8fc0e069
Ian Higgins <ianrhiggins@gmail.com>
2025-08-13 10:01:39 -0700

    edited bulk upload dump structure to handle projects

--- temperature_data.py
+++ temperature_data.py
@@ -36,25 +36,27 @@ config = configparser.ConfigParser()
 config.read('config\gdata_config.ini')
 
 parameter = "groundwater_level"
+project = "vashon"
 
 def import_from_folder():
     # Define the folder path
-    folder_path = Path(r"bulk_upload")
+    folder_path = Path(f"bulk_upload\{project}")
 
     # List all subfolders
     subfolders = [subfolder for subfolder in folder_path.iterdir() if subfolder.is_dir()]
-  
+    print(subfolders)
 
     # iterate over sub foulders
     for subfolder in subfolders:
         site = subfolder.name
         site_sql_id = get_site_sql_id(site)
-        print(f"folder {subfolder.name}, site {site}")
+        print(f"folder {subfolder.name}, site {site}, site sql id {site_sql_id}")
         folder_path = subfolder
             # List all files in the folder
             # List all CSV files in the folder
         files = [file for file in folder_path.iterdir() if file.is_file() and file.suffix == '.csv']
-        
+        print("files")
+        print(files)
             
         # create blank df
         data = pd.DataFrame(columns=['site', 'site_sql_id', 'datetime', 'water_temperature', 'session'])
@@ -63,26 +65,29 @@ def import_from_folder():
         for file in files:
             try:
                 print("file: ",  file)
-                df = pd.read_csv(file, skiprows=1, usecols=[1,3], parse_dates=[0]) # Adjust format as needed)  # Skip first row and ignore the first column
+                #df = pd.read_csv(file, skiprows=1, usecols=[1,3]) # Adjust format as needed)  # Skip first row and ignore the first column , parse_dates=[0]
+                df = pd.read_csv(file, usecols=[0,2])
                         # clean column names 
                 
                 df.columns = df.columns.str.replace(r"\s*\(.*?\)", "", regex=True)
                 df.columns = df.columns.str.replace(",", "", regex=False)
                 df = df.rename(columns={df.columns[0]: 'datetime'})
+                df['datetime'] = pd.to_datetime(df['datetime'])
                 if df.columns[1] == "Temp °F": # if in F convert to c
                             df["Temp °F"] = (df["Temp °F"] - 32) / 1.8
 
                 df = df.rename(columns={df.columns[1]: 'water_temperature'})  
                     
-                
+              
                 df = df.dropna()
-               
+                print("df")
+                print(df.head(5))
                 # conver to deg c if need be
                 if df["water_temperature"].mean() > 30:  # f
                             df["water_temperature"] = round((df["water_temperature"] - 32) * (5/9), 2)
                 #### remove outliers on first and last 12 rows (3 hours)
                 # get statistics
-        
+             
                 mean_head = df.loc[df.index[:11], "water_temperature"].mean().round(2)
                 mean_tail = df.loc[df.index[-10:], "water_temperature"].mean().round(2)
 
@@ -106,15 +111,18 @@ def import_from_folder():
                 # fill values               
                 df["water_temperature"] = df["water_temperature"].interpolate(method="linear", limit=4, limit_direction = "both")
                 
-                
-                
+               
 
                 ### resample to 15 minute intervals
                 # 3. Interpolate water_temperature (already done)
                 df = df.set_index("datetime")
-                
-                df = df.resample("15T").mean()
+               
+                df = df.resample("1H").mean()
+                print("df")
+                print(df.head(5))
                 df.reset_index(drop=False, inplace = True)
+                
+                
                 df["water_temperature"] = df["water_temperature"].interpolate(method="linear", limit_direction="both")
                 df['water_temperature'] = df['water_temperature'].round(2)
                 df["site"] = site
@@ -149,9 +157,9 @@ def import_from_folder():
 
         ### meta data
         # make a metadata foulder
-        os.makedirs(f"bulk_upload\{site}\metadata", exist_ok=True)
+        os.makedirs(f"bulk_upload\{project}\{site}\metadata", exist_ok=True)
         # save file to metadata
-        data.to_csv(f"bulk_upload\{site}\metadata\{site}_data.csv", index=False )
+        data.to_csv(f"bulk_upload\{project}\{site}\metadata\{site}_data.csv", index=False )
         # create a graph
         # Start a new plot
         plt.figure(figsize=(10, 5))
@@ -168,14 +176,14 @@ def import_from_folder():
         plt.legend(title="Session", bbox_to_anchor=(1.05, 1), loc='upper left')
         plt.grid(False)
         plt.tight_layout()
-        plt.savefig(f"bulk_upload\{site}\metadata\{site} water temperature.pdf")
+        plt.savefig(f"bulk_upload\{project}\{site}\metadata\{site} water temperature.pdf")
         #plt.show()
 
 
 
 def upload_data():
     """ runs seperatly  to allow for checking of data"""
-    folder_path = Path(r"bulk_upload")
+    folder_path = Path(f"bulk_upload\{project}")
 
    
     subfolders = [subfolder for subfolder in folder_path.iterdir() if subfolder.is_dir()]
@@ -185,7 +193,7 @@ def upload_data():
 
         print(subfolder)
         site = subfolder.name
-        data = pd.read_csv(f"bulk_upload\{site}\metadata\{site}_data.csv")
+        data = pd.read_csv(f"bulk_upload\{project}\{site}\metadata\{site}_data.csv")
         data["datetime"] = pd.to_datetime(data["datetime"])
         print(data)
         update_15_minute_data(data, site, parameter)
@@ -194,5 +202,5 @@ def upload_data():
         print("daily values complete")
    
 
-#import_from_folder()
+import_from_folder()
 upload_data()
